855fb6ba6830c1cae867004e853f2a8011d36ad8
SibhatY <57141058+SibhatY@users.noreply.github.com>
2024-06-18 16:53:18 -0500

    Working UI for complete game, will add reset/game end logic

--- rps.js
+++ rps.js
@@ -3,6 +3,9 @@ let computerScore = 0;
 let humanScore = 0;
 let rounds = 0;
 
+
+
+
 function getComputerChoice() {
 
     let randNum = Math.floor(Math.random() * 3);
@@ -30,19 +33,28 @@ function playRound(computerChoice, humanChoice) {
     console.log(`You chose ${humanChoice}`);
 
 
+    let gameMessage;
 
     if ((humanChoice === "Rock" && computerChoice === "Paper") || (humanChoice === "Paper" && computerChoice === "Scissors") || (humanChoice === "Scissors" && computerChoice === "Rock")) {
 
         computerScore += 1;
+
         console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+
+        gameMessage = `You Lose! ${computerChoice} beats ${humanChoice}`;
+
 
     } else if ((humanChoice === "Rock" && computerChoice === "Scissors") || (humanChoice === "Paper" && computerChoice === "Rock") || (humanChoice === "Scissors" && computerChoice === "Paper")) {
 
         humanScore += 1;
+
         console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+
+        gameMessage = `You Win! ${humanChoice} beats ${computerChoice}`;
 
     } else {
         console.log("It's a Tie!");
+        gameMessage = "It's a Tie!";
     }
 
 
@@ -51,8 +63,25 @@ function playRound(computerChoice, humanChoice) {
 
     rounds += 1;
 
+    updateDOM(gameMessage);
+
     updateGame();
 
+
+
+
+}
+
+
+function updateDOM(message) {
+
+    const score = document.querySelector("#score");
+    const status = document.querySelector("#game");
+    const roundInfo = document.querySelector("#roundInfo");
+
+    score.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`;
+
+    roundInfo.textContent = `Round ${rounds}: ${message}`;
 
 }
 
@@ -60,15 +89,24 @@ function playRound(computerChoice, humanChoice) {
 
 
 function updateGame() {
+    
+    const status = document.querySelector("#status");
+
 
     if (humanScore >= 5 || computerScore >= 5) {
 
         if (humanScore > computerScore) {
             console.log("YOU WIN THE GAME!!!");
+            status.textContent = "GAME OVER! YOU WIN!";
         }
         else {
             console.log("AWW, YOU LOST THE GAME!");
+            status.textContent = "GAME OVER! YOU LOSE!";
         }
+        //Game end logic here to stop game or reset
+    }
+    else {
+        status.textContent = `Choose your move!`;
     }
 }
 
